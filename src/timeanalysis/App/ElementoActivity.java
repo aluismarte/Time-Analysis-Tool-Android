fcc99fce07a10e03dd4eb06a69f978c5b172e96d
aluismarte <aluismarte@gmail.com>
2012-12-16 14:56:05 -0400

    Definicion de XML y integracion de las estructuras

--- src/timeanalysis/App/ElementoActivity.java
+++ src/timeanalysis/App/ElementoActivity.java
@@ -8,7 +8,7 @@ public class ElementoActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_elemento);
 	}
 	
 }
