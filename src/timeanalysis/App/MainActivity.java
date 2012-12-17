5edb5dbf9f72c6fc508d9901ee57b768c7bf7032
aluismarte <aluismarte@gmail.com>
2012-12-17 19:43:10 -0400

    Modificacion de GUI, Manejo de operaciones y elementos mediante una base de datos local, Modelos de datos para operaciones y elementos, Adicion del cronometro y funcionalidad, Mejora de manejo de las tostatas(pop up) y mejoras a utilerias.

--- src/timeanalysis/App/MainActivity.java
+++ src/timeanalysis/App/MainActivity.java
@@ -1,19 +1,42 @@
 package timeanalysis.App;
 
+import timeanalysis.App.Adaptadores.AdaptadorDatos;
+import android.app.TabActivity;
+import android.content.res.Resources;
+import android.content.Intent;
 import android.os.Bundle;
-import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.GridView;
+import android.widget.TabHost;
+import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
+import android.widget.Toast;
 
 /*
 Aqui se elige el tipo de salvado
 */
 
-public class MainActivity extends Activity {
+@SuppressWarnings("deprecation")
+public class MainActivity extends TabActivity {
+	
+	private GridView gridView;
+	
+	private String[] Datos;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		setContentView(R.layout.activity_captura);
+		
+		//Preparo tabs a la antigua
+		
+		
+		//Visor de la data;
+		PrepararView();
+		
 	}
 	
 	@Override
@@ -21,6 +44,42 @@ public class MainActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
+	}
+	
+	public void PrepararView() {
+		gridView = (GridView) findViewById(R.id.capturaDatos);
+		
+		AdaptadorDatos adapter = new AdaptadorDatos(this, Datos);
+		
+		gridView.setAdapter(adapter);
+		
+		gridView.setOnItemClickListener(new OnItemClickListener() {
+			public void onItemClick(AdapterView<?> parent, View v,int position, long id) {
+				Toast.makeText(getApplicationContext(),((TextView) v.findViewById(R.id.grid_item_label)).getText(), Toast.LENGTH_SHORT).show();
+			}
+		});
+	}
+	
+	public void PrepararTabs() {
+		Resources ressources = getResources(); 
+		TabHost tabHost = getTabHost();
+		
+		Intent intentAndroid = new Intent().setClass(this, OperacionActivity.class);
+		
+		TabSpec tabSpecAndroid = tabHost
+				.newTabSpec("Operaciones")
+				.setIndicator("", ressources.getDrawable(R.drawable.))
+				.setContent(intentAndroid);
+		
+		tabHost.setCurrentTab(0);
+	}
+	
+	public String[] getDatos() {
+		return this.Datos;
+	}
+	
+	public void setDatos(String dat[]) {
+		this.Datos = dat;
 	}
 	
 }
